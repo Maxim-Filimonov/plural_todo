b821923e0617c439a30015073903e850cd315404
Maxim Filimonov <tpaktopsp@gmail.com>
2017-11-17 15:32:55 +0000

    cleanig up

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+
+    };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Text>Hello world!</Text>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
