e07141406019b477f370c99778f79fe380c244aa
Maxim Filimonov <tpaktopsp@gmail.com>
2017-11-17 15:40:57 +0000

    add tasklist component

--- App.js
+++ App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import {
   Text,
 } from 'react-native';
+import TaskList from './TaskList';
 
 export default class PluralTodo extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-
+      todos: [
+        { task: 'Finish Pluralsight Tutorial' },
+        { task: 'Cleanup NatRhythms App component' },
+      ],
     };
   }
   render() {
     return (
-      <Text>Hello world!</Text>
+      <TaskList />
     );
   }
 }
---
+++ TaskList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import {
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+  },
+});
+class TaskList extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hi, this is a tasklist</Text>
+      </View>
+    );
+  }
+}
+export default TaskList;
