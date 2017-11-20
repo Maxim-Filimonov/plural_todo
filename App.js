3ccdd66c4862742eb08aff080ca6df01a69aa908
Maxim Filimonov <tpaktopsp@gmail.com>
2017-11-20 20:26:25 +0000

    Using HLC function to deal with props passing Stackoverflow#47398607 react-navigation#740

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, NavigationActions } from 'react-navigation';
 
-import Home from './Home';
-import TaskFormScreen from './TaskForm';
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
 
-
-const Nav = StackNavigator({
-  TaskList: {
-    screen: Home,
-  },
-  TaskForm: {
-    screen: TaskFormScreen,
-  },
-});
+const createComponent = (instance, props) =>
+  navProps => React.createElement(instance, Object.assign({}, props, navProps));
 
 export default class PluralTodo extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      tasks: [
+        { name: 'Finish Pluralsight Tutorial..' },
+        { name: 'Cleanup NatRhythms App component..' },
+      ],
+    };
+    this.onAddStarted = this.onAddStarted.bind(this);
+    this.onAddCancel = this.onAddCancel.bind(this);
+    this.onAddSubmit = this.onAddSubmit.bind(this);
+  }
+  onAddCancel() {
+    this.navigator.dispatch(NavigationActions.back({}));
+  }
+  onAddSubmit() {
+    console.log('Submitting!');
+  }
+  onAddStarted() {
+    this.navigator.dispatch(NavigationActions.navigate({ routeName: 'TaskForm' }));
+  }
   render() {
+    const Nav = StackNavigator({
+      TaskList: {
+        screen: createComponent(TaskList, { tasks: this.state.tasks, onAddStarted: this.onAddStarted }),
+      },
+      TaskForm: {
+        screen: createComponent(TaskForm, { onCancel: this.onAddCancel, onAdd: this.onAddSubmit }),
+      },
+    });
     return (
       <Nav ref={nav => this.navigator = nav} />
     );
